05a8301d9319444da9a54495425de791222d154b
ymc-frne <frank.neff@ymc.ch>
2014-03-13 12:57:34 +0100

    File check for XML

---
+++ src/ZendDiagnostics/Check/XmlFile.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ZendDiagnostics\Check;
+
+use XMLReader;
+use ZendDiagnostics\Result\Failure;
+use ZendDiagnostics\Result\Success;
+
+/**
+ * Checks if an XML file is available and valid
+ */
+class XmlFile extends AbstractFileCheck
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function validateFile($file)
+    {
+        $xmlReader = new XMLReader();
+
+        if (!$xmlReader->open($file)) {
+            return new Failure(sprintf('Could not open "%s" with XMLReader!', $file));
+        }
+
+        $xmlReader->setParserProperty(XMLReader::VALIDATE, true);
+
+        if (!$xmlReader->isValid()) {
+            return new Failure(sprintf('File "%s" is not valid XML!', $file));
+        }
+
+        if (!@simplexml_load_file($file)) {
+            return new Failure(sprintf('File "%s" is not well-formed XML!', $file));
+        }
+
+        return new Success();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLabel()
+    {
+        return 'XML file check';
+    }
+} 
